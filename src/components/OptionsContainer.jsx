4e23eb5229625c8ec4ed97c2c1f99a413c1abd86
MikDra1 <mikolajkuleczka@gmail.com>
2025-01-16 18:26:48 +0100

    clean OptionsContainer component

--- src/components/OptionsContainer.jsx
+++ src/components/OptionsContainer.jsx
@@ -1,30 +1,9 @@
-import styled from "styled-components";
-import Option from "./Option";
 import { useGame } from "../context/GameProvider";
 import MobileOptionsContainer from "./MobileOptionsContainer";
 import DesktopOptionsContainer from "./DesktopOptionsContainer";
 
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  position: relative;
-  width: 100%;
-  max-width: 700px;
-  margin-inline: auto;
-  padding: 3rem 0;
-  background-image: url("../images/bg-triangle.svg");
-  background-repeat: no-repeat;
-  background-position: top 70% left 50%;
-  background-size: 70%;
-  flex: 1;
-  height: 100%;
-
-
-
-`;
-
 function OptionsContainer() {
-  const {  isMobile } = useGame();
+  const { isMobile } = useGame();
 
   return (
     <>
